1a6b2a877d07fa70a3e1412105d4b1d4108c31f9
Mahmoud Shaaban <mahshaban95@gmail.com>
2022-07-07 03:23:41 +0000

    add docker provider and pull autograder image

--- terraform-aws-ecs/main.tf
+++ terraform-aws-ecs/main.tf
@@ -4,7 +4,18 @@ terraform {
       source = "hashicorp/aws"
       version = "4.21.0"
     }
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.17.0"
+    }
   }
+}
+
+provider "docker" {
+}
+# Pulls the image from docker hub
+resource "docker_image" "autograder" {
+    name = "mahshaban95/autograder:2.0"
 }
 
 provider "aws" {
